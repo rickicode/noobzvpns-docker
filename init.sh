04874ee2eee9923c2241337256f2103b49dc757a
rickicode <rickicode@gmail.com>
2024-09-10 00:22:27 +0700

    Refactor Dockerfile: Update file paths, add init.sh, and modify entrypoint

--- init.sh
+++ init.sh
@@ -9,23 +9,19 @@ echo "Downloading Config..."
 # Download file konfigurasi dan sertifikat
 wget -q -O /etc/noobzvpns/cert.pem https://0ms.dev/mirrors/raw.githubusercontent.com/rickicode/noobzvpns-docker/main/noobz/cert.pem
 wget -q -O /etc/noobzvpns/key.pem https://0ms.dev/mirrors/raw.githubusercontent.com/rickicode/noobzvpns-docker/main/noobz/key.pem
-wget -q -O /etc/noobzvpns/config.json https://0ms.dev/mirrors/raw.githubusercontent.com/rickicode/noobzvpns-docker/main/noobz/config.json
+wget -q -O /etc/noobzvpns/config.json https://raw.githubusercontent.com/rickicode/noobzvpns-docker/main/noobz/config.json
 
 # Cek dan update PORT_TLS jika variabel lingkungan tersedia
 if [ -n "$PORT_TLS" ] && [[ "$PORT_TLS" =~ ^[0-9]+$ ]]; then
     echo "Mengubah PORT_TLS di config.json menjadi $PORT_TLS"
     # Ubah PORT_TLS di config.json
     sed -i "s/4433/$PORT_TLS/g" /etc/noobzvpns/config.json
-else
-    echo "PORT_TLS tidak valid atau tidak diatur. Tidak melakukan perubahan pada config.json."
 fi
 
 if [ -n "$PORT_NTLS" ] && [[ "$PORT_NTLS" =~ ^[0-9]+$ ]]; then
     echo "Mengubah PORT_TLS di config.json menjadi $PORT_NTLS"
     # Ubah PORT_NTLS di config.json
     sed -i "s/8800/$PORT_NTLS/g" /etc/noobzvpns/config.json
-else
-    echo "PORT_NTLS tidak valid atau tidak diatur. Tidak melakukan perubahan pada config.json."
 fi
 
 if [ ! -f "/usr/bin/noobz" ]; then
